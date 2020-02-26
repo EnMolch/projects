8ffb06bdc9cb37407a072018654850f076be4b7e
EnMolch <61197734+EnMolch@users.noreply.github.com>
2020-02-26 20:57:22 +0100

    Alle Funktionalitäten bis auf Dateispeciherung und REchtschreibung

--- Python/Snacc_Obj.py
+++ Python/Snacc_Obj.py
@@ -6,17 +6,21 @@ Produkt_Namen = ["Mars","Molch", "Schrödigners Katze", "Sicke plays", "Pornobal
 Produkt_Preise = [1.2, 1.3, 1, 0.5, 3.5, 0.7, 0.6, 4.20, 0.99, 3]
 Produkt_Anzahl = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 Nutzer_Daten = []
-Produkt_Array = []
+Produkt_Array = [0,0,0,0,0,0,0,0,0,0]
 class Produkte:
 
     def __init__(self, Name, Preis, Anzahl):
         self.Name = Name
         self.Preis = Preis
         self.Anzahl = Anzahl
-    def Lager(self):
+    def GetLager(self):
         return self.Anzahl
-    def Kosten(self):
+    def GetKosten(self):
         return self.Preis
+    def SetLager(self, Anzahl):
+        self.Anzahl = Anzahl
+    def GetName(self):
+        return self.Name
 
 class Nutzer:
 
@@ -70,8 +74,6 @@ class Nutzer:
         
                     
 
-
-
 class Snacc:
 
     def __init__(self):
@@ -87,7 +89,7 @@ class Snacc:
         Kontostand = ID.Get_Konto()
 
         try:
-            Konto_Cache = round(float(input("Wie viel wollen sie einzahlen?")), 2)
+            Konto_Cache = round(float(input("Wie viel wollen sie einzahlen?  ")), 2)
 
             if Konto_Cache > 0:
                 Kontostand = Kontostand + Konto_Cache
@@ -99,15 +101,18 @@ class Snacc:
         except ValueError:
             print("Geben sie nur gültige Werte ein! \n")
     
-    def Kaufen(self, Name, Preis, Stonks, Bname):
-        if Stonks >0:    
-            if Preis < Bname.Get_Konto:
-                Bname.set_Konto(Bname.Get_Konto - Preis)
+    def Kaufen(self, Name, Preis, Stonks, ID):
+        if Stonks >0:
+            Kontostand = ID.Get_Konto()   
+            print(Kontostand) 
+            if Preis < Kontostand:
+                ID.Set_Konto(Kontostand - Preis)
                 print(f"viel spass mit {Name}")
                 Produkt_Anzahl[Produkt_Namen.index(Name)] = Produkt_Anzahl[Produkt_Namen.index(Name)] -1
-    
+            else:
+                print("Sieh den Monopolkapitalist, der dich mit Haut und Haaren frist!")
     def Produkt_Init(self):
-        for i in range(0,len(Produkt_Namen), 3):
+        for i in range(len(Produkt_Namen)):
             Produkt_Array[i] = Produkte(Produkt_Namen[i], Produkt_Preise[i], Produkt_Anzahl[i])
 
 
@@ -117,38 +122,47 @@ if __name__ == "__main__":
         Bname_In = str(input("Username: "))
         Pw_In = str(input("Passwort: "))
         try:
-            Reg_Einlog = int(input("Einloggen [0]   Registrieren[1]"))
+            Reg_Einlog = int(input("Einloggen [0]   Registrieren[1]  "))
             print(Reg_Einlog)
+            User_Identity = Nutzer(Bname_In, Pw_In)            #Fallunterscheidung in Neu oder Alt
+
+            if Reg_Einlog >0:
+                User_Identity.Registrieren()
+                Automat_Instanz = Snacc()
+                #Automat_Instanz.Produkt_Init()
+
+            else: 
+                User_Identity.Einloggen()
+                Automat_Instanz = Snacc()
+                #Automat_Instanz.Produkt_Init()
+
+            while (User_Identity.Logged_In):
+                Automat_Instanz.Produkt_Init()
+                Input_Cache = input("""Willkommen beim Snacc!
+                Was wollen sie tun?
+                    Einzahlen   [1]
+                    Kaufen      [2]
+                    Beenden     [3]  """)
+
+                if Input_Cache == "1":
+                    Automat_Instanz.Einzahlung(User_Identity)
+                    
+
+                elif Input_Cache == "2":
+                    for i in range(len(Produkt_Array)):
+                        print(Produkt_Array[i].GetName(),"    ", Produkt_Array[i].GetKosten(),"    " ,Produkt_Array[i].GetLager())
+                    Name_Buy = str(input("Bitte den Namen des gewünschten Produktes eingeben: "))
+                    
+                    if Name_Buy in Produkt_Namen:
+                        Temp_Index = Produkt_Namen.index(Name_Buy)
+                        Temp_Preise = Produkt_Preise[Temp_Index]
+                        Temp_Anzahl = Produkt_Anzahl[Temp_Index]
+                        Automat_Instanz.Kaufen(Name_Buy, Temp_Preise , Temp_Anzahl, User_Identity)
+
+                    else:
+                        print("Bitte korrekten Produktnamen eingeben")
+
+                elif Input_Cache == "3":
+                    User_Identity.Ausloggen()
         except ValueError:
             print("Keine valide Option")
-        User_Identity = Nutzer(Bname_In, Pw_In)            #Fallunterscheidung in Neu oder Alt
-
-        if Reg_Einlog:
-            User_Identity.Registrieren()
-            Automat_Instanz = Snacc()
-            Automat_Instanz.Produkt_Init()
-
-        else: 
-            User_Identity.Einloggen()
-            Automat_Instanz = Snacc()
-            Automat_Instanz.Produkt_Init()
-
-        while (User_Identity.Logged_In):
-            Automat_Instanz.Produkt_Init()
-            Input_Cache = input("""Willkommen beim Snacc!
-            Was wollen sie tun?
-                Einzahlen   [1]
-                Kaufen      [2]
-                Beenden     [3] """)
-
-            if Input_Cache == "1":
-                Automat_Instanz.Einzahlung(User_Identity)
-                
-
-            elif Input_Cache == "2":
-                for i in Produkt_Array:
-                    print(i)
-                #Automat_Instanz.Kaufen()
-
-            elif Input_Cache == "3":
-                User_Identity.Ausloggen()
